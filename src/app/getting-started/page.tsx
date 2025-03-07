f08f64eec776ec24687fd662703dabb873ec4170
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-03-07 09:57:05 -0600

    Update page.tsx

--- src/app/getting-started/page.tsx
+++ src/app/getting-started/page.tsx
@@ -1,3 +1,11 @@
 export default function GettingStarted() {
-  return <main>Getting Started</main>;
+  return (
+    <main className="transition-all text-center">
+      <div>
+        <h1 className="mt-12 text-4xl font-semibold">Coming Soon!</h1>
+        <p className="mt-2 text-2xl"></p>
+      </div>
+      <div className="mt-8 text-lg"></div>
+    </main>
+  );
 }
