c7299437ca0160c03f30f82aa01f7538a5e4d716
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-04-14 23:55:02 -0500

    Update page.tsx

--- src/app/getting-started/page.tsx
+++ src/app/getting-started/page.tsx
@@ -2,7 +2,7 @@ export default function GettingStarted() {
   return (
     <main className="transition-all text-center">
       <div>
-        <h1 className="mt-12 text-4xl font-semibold">Coming Soon!</h1>
+        <h1 className="mt-12 text-4xl font-semibold">Coming Soon..</h1>
         <p className="mt-2 text-2xl"></p>
       </div>
       <div className="mt-8 text-lg"></div>
