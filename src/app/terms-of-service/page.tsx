43f51370c37720379d98ff46540573657bf99fb9
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-05-19 01:00:22 -0500

    Onboarding

--- src/app/terms-of-service/page.tsx
+++ src/app/terms-of-service/page.tsx
@@ -3,9 +3,7 @@ export default function TermsOfService() {
   return (
     <main>
       <div className="text-center mt-8">
-        <h1 className="text-neutral-100 font-semibold text-2xl">
-          Terms Of Service
-        </h1>
+        <h1 className="text-neutral-100 font-semibold text-2xl"></h1>
         <p className="block text-center mt-4 mx-auto w-full max-w-screen-sm text-wrap"></p>
       </div>
     </main>
