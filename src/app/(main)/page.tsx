c594b750af9e711d6c982c94cd2a3df2b82cb216
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-05-25 00:39:31 -0500

    finish font refactor

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -13,17 +13,17 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-background text-zinc-100 flex flex-col items-center justify-center p-8 gap-300 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen bg-background text-zinc-100 flex flex-col items-center justify-center p-8 gap-300 sm:p-20">
       <main className="w-full max-w-[1400px] mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 items-center">
           <div className="flex flex-col gap-4 items-center md:items-start text-left md:col-span-2">
-            <h1 className="font-medium text-zinc-100 font-[family-name:var(--font-ginto-nord)] text-center md:text-left text-4xl md:text-7xl">
+            <h1 className="text-zinc-100 font-gintoNord text-center md:text-left text-4xl md:text-7xl">
               <TextEffect per="char" speedReveal={3} delay={0.1}>
                 Automating the busywork of Discord community management
               </TextEffect>
             </h1>
-            <div className="text-left text-zinc-300 font-[family-name:var(--font-ginto-regular)] md:translate-x-0 translate-x-7 font-regular text-2xl md:text-4xl space-y-4">
-              <div className="font-regular text-sm text-center md:text-left md:text-xl max-w-5/6 md:max-w-4/6">
+            <div className="text-left text-zinc-300 font-ginto md:translate-x-0 translate-x-7 text-2xl md:text-4xl space-y-4">
+              <div className="text-sm text-center md:text-left md:text-xl max-w-5/6 md:max-w-4/6">
                 <TextEffect per="line" speedReveal={1} delay={1}>
                   Arclify sets dynamic nicknames, personalizes identities, and
                   streamlines community workflows - Make Discord management
@@ -37,7 +37,7 @@ export default function Home() {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 0.5, delay: 1.5 }}
-                className="cursor-pointer font-medium text-black font-[family-name:var(--font-ginto-medium)] text-sm md:text-base bg-zinc-100 px-6 py-3 rounded-lg shadow-md hover:bg-neutral-400 transition duration-300"
+                className="cursor-pointer font-medium text-black font-ginto text-sm md:text-base bg-zinc-100 px-6 py-3 rounded-lg shadow-md hover:bg-neutral-400 transition duration-300"
               >
                 Sign up for free early access
               </motion.button>
