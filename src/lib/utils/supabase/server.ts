c14fa9136ea470928297e421164d2e426c6b46c8
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-05-27 03:28:13 -0500

    initial supabase auth setup with email

--- src/lib/utils/supabase/server.ts
+++ src/lib/utils/supabase/server.ts
@@ -4,7 +4,7 @@ export async function createClient() {
   const cookieStore = await cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY!,
     {
       cookies: {
         getAll() {
