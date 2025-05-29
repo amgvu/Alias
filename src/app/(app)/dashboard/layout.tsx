29cc98898c41e873f24231766ab121ddde9b1e83
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-05-29 01:23:41 -0500

    fixed font routes

--- src/app/(app)/dashboard/layout.tsx
+++ src/app/(app)/dashboard/layout.tsx
@@ -1,5 +1,62 @@
 "use client";
 import ClientProvider from "@/components/ClientProvider";
+import localFont from "next/font/local";
+
+const gintoNord = localFont({
+  src: [
+    {
+      path: "../../../../public/fonts/ABCGintoDiscordNord-Bold.woff2",
+      weight: "700",
+      style: "bold",
+    },
+    {
+      path: "../../../../public/fonts/ABCGintoDiscordNord-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../../../public/fonts/ABCGintoDiscordNord-BlackItalic.woff2",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-gintoNord",
+  display: "swap",
+  adjustFontFallback: "Arial",
+  preload: true,
+});
+
+const ginto = localFont({
+  src: [
+    {
+      path: "../../../../public/fonts/ABCGintoDiscord-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../../../public/fonts/ABCGintoDiscord-Medium.woff2",
+      weight: "500",
+      style: "medium",
+    },
+  ],
+  variable: "--font-ginto",
+  display: "swap",
+  adjustFontFallback: "Arial",
+  preload: true,
+});
+
+const ggSans = localFont({
+  src: [
+    {
+      path: "../../../../public/fonts/ggsansvf-VF.woff2",
+      weight: "400 600",
+    },
+  ],
+  variable: "--font-ggSans",
+  display: "swap",
+  adjustFontFallback: "Arial",
+  preload: true,
+});
 
 export default function DashboardLayout({
   children,
@@ -9,7 +66,11 @@ export default function DashboardLayout({
   return (
     <div className="min-h-screen">
       <ClientProvider>
-        <main>{children}</main>
+        <body
+          className={`${gintoNord.variable} ${ginto.variable} ${ggSans.variable} antialiased w-full h-full`}
+        >
+          <main>{children}</main>
+        </body>
       </ClientProvider>
     </div>
   );
