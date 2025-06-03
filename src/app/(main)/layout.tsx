adcf380c637b997ad0cf50b4e54f1f355bd42b2b
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-06-03 01:43:52 -0500

    refactor

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,75 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import localFont from "next/font/local";
+import { gintoNord, ginto, ggSans } from "@/lib/utilities/fonts";
 import ClientProvider from "@/contexts/ClientProvider";
 import "../globals.css";
 import Navbar from "@/components/ui/navbar/navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const gintoNord = localFont({
-  src: [
-    {
-      path: "../../../public/fonts/ABCGintoDiscordNord-Bold.woff2",
-      weight: "700",
-      style: "bold",
-    },
-    {
-      path: "../../../public/fonts/ABCGintoDiscordNord-BoldItalic.woff2",
-      weight: "700",
-      style: "italic",
-    },
-    {
-      path: "../../../public/fonts/ABCGintoDiscordNord-BlackItalic.woff2",
-      weight: "900",
-      style: "italic",
-    },
-  ],
-  variable: "--font-gintoNord",
-  display: "swap",
-  adjustFontFallback: "Arial",
-  preload: true,
-});
-
-const ginto = localFont({
-  src: [
-    {
-      path: "../../../public/fonts/ABCGintoDiscord-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../../public/fonts/ABCGintoDiscord-Medium.woff2",
-      weight: "500",
-      style: "medium",
-    },
-  ],
-  variable: "--font-ginto",
-  display: "swap",
-  adjustFontFallback: "Arial",
-  preload: true,
-});
-
-const ggSans = localFont({
-  src: [
-    {
-      path: "../../../public/fonts/ggsansvf-VF.woff2",
-      weight: "400 600",
-    },
-  ],
-  variable: "--font-ggSans",
-  display: "swap",
-  adjustFontFallback: "Arial",
-  preload: true,
-});
 
 export const metadata: Metadata = {
   title: "Arclify",
@@ -84,7 +17,7 @@ export default function MainLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${gintoNord.variable} ${ginto.variable} ${ggSans.variable} w-full h-full`}
+        className={`${gintoNord.variable} ${ginto.variable} ${ggSans.variable} w-full h-full`}
       >
         <ClientProvider>
           <Navbar>{children}</Navbar>
