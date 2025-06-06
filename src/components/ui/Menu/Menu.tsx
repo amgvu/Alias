4806038b9db93e07d2e664c9c5972be1f173db82
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-06-06 04:10:05 -0500

    Update Menu.tsx

--- src/components/ui/Menu/Menu.tsx
+++ src/components/ui/Menu/Menu.tsx
@@ -50,11 +50,11 @@ const DSMenu: React.FC<DSMenuProps> = ({
       <Transition
         as={Fragment}
         enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
+        enterFrom="transform opacity-0 scale-100"
         enterTo="transform opacity-100 scale-100"
         leave="transition ease-in duration-75"
         leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
+        leaveTo="transform opacity-0 scale-100"
       >
         <Menu.Items className={menuItemsStyles}>
           <div className="py-1">
