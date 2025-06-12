3abbf15d325497d244ef44ed77899a2b61a6b3bd
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-06-12 02:29:02 -0500

    Update UserList.tsx

--- src/components/ui/UserList/UserList.tsx
+++ src/components/ui/UserList/UserList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { motion } from "framer-motion";
