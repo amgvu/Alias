e55f0f7b4dc586a4f0ec31e429994de9a16c42c8
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-06-24 09:45:44 -0500

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { gintoNord, ginto, ggSans } from "@/lib/utilities/fonts";
 import "./globals.css";
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
