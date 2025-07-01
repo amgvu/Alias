906dcb4a5e5586dc63143571b86535f39ad7fc4e
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-07-01 04:48:09 -0500

    case sensitive fix?

---
+++ src/components/app/Toolbar/SelectionTool/SelectionTool.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Server } from "@/types/types";
+import { SquareDashedMousePointer } from "lucide-react";
+
+interface SelectionToolProps {
+  selectedServer: Server | null;
+  showCheckboxes: boolean;
+  setShowCheckboxes: (show: boolean) => void;
+}
+
+export default function SelectionTool({
+  selectedServer,
+  showCheckboxes,
+  setShowCheckboxes,
+}: SelectionToolProps) {
+  return (
+    <Button
+      onClick={() => setShowCheckboxes(!showCheckboxes)}
+      className="p-2 ml-3 bg-button border border-border enabled:border-border-active rounded-md disabled:bg-transparent disabled:text-text-disabled disabled:cursor-not-allowed cursor-pointer text-sm sm:text-sm md:text-base lg:text-[15px] xl:text-[16px] 2xl:text-[17px] text-text-primary hover:bg-button-hover"
+      disabled={!selectedServer}
+    >
+      <SquareDashedMousePointer className="w-4 h-4 sm:w-4 md:w-5 md:h-5 2xl:w-6 2xl:h-6 inline-block" />
+      {showCheckboxes ? "Unselect" : "Select"}
+    </Button>
+  );
+}
---
+++ src/components/app/Toolbar/Toolbar.tsx
@@ -0,0 +1,39 @@
+import { Server, Member } from "@/types/types";
+import { SelectionTool, ApplyTool } from "@/components";
+
+interface ToolbarProps {
+  members: Member[];
+  onApplyToSelection: (selectedMembers: Member[]) => void;
+  selectedUserIds: string[];
+  selectedServer: Server | null;
+  showCheckboxes: boolean;
+  setShowCheckboxes: (show: boolean) => void;
+}
+
+export default function Toolbar({
+  members,
+  onApplyToSelection,
+  selectedUserIds,
+  selectedServer,
+  showCheckboxes,
+  setShowCheckboxes,
+}: ToolbarProps) {
+  return (
+    <div>
+      <div className="bg-panel z-4 fixed w-screen translate-x-[111.0px] translate-y-8 p-[9.0px] border-border border-r border-t border-b">
+        <SelectionTool
+          selectedServer={selectedServer}
+          showCheckboxes={showCheckboxes}
+          setShowCheckboxes={setShowCheckboxes}
+        />
+
+        <ApplyTool
+          members={members}
+          onApplyToSelection={onApplyToSelection}
+          selectedUserIds={selectedUserIds}
+          selectedServer={selectedServer}
+        />
+      </div>
+    </div>
+  );
+}
