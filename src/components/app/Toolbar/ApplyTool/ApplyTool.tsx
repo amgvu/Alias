874aeca9180786703854bcd6b231fc9cc35969c0
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-07-03 06:17:50 -0500

    confirmation for apply button

--- src/components/app/Toolbar/ApplyTool/ApplyTool.tsx
+++ src/components/app/Toolbar/ApplyTool/ApplyTool.tsx
@@ -2,6 +2,16 @@ import { Server } from "@/types/types";
 import { CheckCheck } from "lucide-react";
 import { Member } from "@/types/types";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogFooter,
+  DialogClose,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 interface ApplyToolProps {
   members: Member[];
@@ -25,13 +35,39 @@ export default function ApplyTool({
   };
 
   return (
-    <Button
-      onClick={handleApply}
-      className="p-2 bg-button ml-2 text-sm sm:text-sm md:text-base lg:text-[15px] xl:text-[16px] 2xl:text-[17px] border border-border enabled:border-border-active rounded-md disabled:bg-transparent disabled:text-text-disabled disabled:cursor-not-allowed cursor-pointer text-text-primary hover:bg-button-hover"
-      disabled={!selectedServer || selectedUserIds.length === 0}
-    >
-      <CheckCheck className="w-4 h-4 sm:w-4 md:w-5 md:h-5 2xl:w-6 2xl:h-6 inline-block" />
-      Apply
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          className="p-2 bg-button ml-2 text-sm sm:text-sm md:text-base lg:text-[15px] xl:text-[16px] 2xl:text-[17px] border border-border enabled:border-border-active rounded-md disabled:bg-transparent disabled:text-text-disabled disabled:cursor-not-allowed cursor-pointer text-text-primary hover:bg-button-hover"
+          disabled={!selectedServer || selectedUserIds.length === 0}
+        >
+          <CheckCheck className="w-4 h-4 sm:w-4 md:w-5 md:h-5 2xl:w-6 2xl:h-6 inline-block" />
+          Apply
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-text-primary">
+            Apply {selectedUserIds.length} nicknames
+          </DialogTitle>
+          <DialogDescription>
+            Are you sure you want to apply the nicknames to{" "}
+            {selectedUserIds.length} members in the server?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button
+              onClick={handleApply}
+              className="p-2 bg-button ml-2 text-sm sm:text-sm md:text-base lg:text-[15px] xl:text-[16px] 2xl:text-[17px] border border-border enabled:border-border-active rounded-md disabled:bg-transparent disabled:text-text-disabled disabled:cursor-not-allowed cursor-pointer text-text-primary hover:bg-button-hover"
+              disabled={!selectedServer || selectedUserIds.length === 0}
+            >
+              <CheckCheck className="w-4 h-4 sm:w-4 md:w-5 md:h-5 2xl:w-6 2xl:h-6 inline-block" />
+              Yes, apply {selectedUserIds.length} nicknames
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
