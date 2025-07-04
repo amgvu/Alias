fc554d236cc5591b9e303bb822c7485796bc56ff
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-07-04 04:55:45 -0500

    Update page.tsx

--- src/app/(main)/features/page.tsx
+++ src/app/(main)/features/page.tsx
@@ -10,7 +10,10 @@ export default function Features() {
         animate={{ opacity: 1 }}
         transition={{ duration: 0.2, delay: 0.1 }}
       >
-        <h1 className="text-3xl text-neutral-200 font-bold mb-2">WIP</h1>
+        <h1 className="text-3xl text-neutral-200 font-bold mb-2">
+          This page is a work in progress (still) but don&apos;t worry, features
+          do exist.
+        </h1>
       </motion.div>
     </main>
   );
