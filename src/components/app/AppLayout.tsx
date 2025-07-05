350d801053b84f2ad97049c76b2036fac44b2d37
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-07-05 04:56:06 -0500

    sonner styling

--- src/components/app/AppLayout.tsx
+++ src/components/app/AppLayout.tsx
@@ -17,11 +17,11 @@ export default function AppLayout({
     <div className="min-h-screen font-ggSans text-white bg-background">
       <div className="flex">
         <TitleUpdater />
+        <Toaster />
         <div className="flex-shrink-0 fixed z-8">{authcard}</div>
         <div className="flex-shrink-0 z-5">{sidebar}</div>
         <div>{topbar}</div>
         <div className="flex flex-col flex-1 ml-10">{servercontent}</div>
-        <Toaster />
       </div>
     </div>
   );
