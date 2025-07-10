d157f48948d9002e48508a8fa10726cc91c01ebf
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-07-10 07:18:45 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['cdn.discordapp.com']
+    domains: ["cdn.discordapp.com"],
   },
+  transpilePackages: ["framer-motion"],
 };
 
 export default nextConfig;
