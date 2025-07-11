13bf52a28fa6450376b570e8d188dd5de81276ae
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-07-11 06:51:16 -0500

    fix build

--- src/components/app/SideTopBar/GroupsPanel/GroupsPanelChildren/VirtualizedNicknameList.tsx
+++ src/components/app/SideTopBar/GroupsPanel/GroupsPanelChildren/VirtualizedNicknameList.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import { useRef } from "react";
 
-import { VirtualizedNicknameListProps } from "@/components/app/SideTopBar/groups/GroupsPanelChildren/VirtualizedNicknameList.types";
+import { VirtualizedNicknameListProps } from "@/components/app/SideTopBar/GroupsPanel/GroupsPanelChildren/VirtualizedNicknameList.types";
 
 export default function VirtualizedNicknameList({
   nicknames,
