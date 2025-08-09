ea37a4cbe06e19acd0bf241d43e23e7cb65eb81b
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-08-09 09:27:01 -0500

    Update page.tsx

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,14 +3,20 @@ import Hero from "@/components/landing/hero/hero";
 import Features from "@/components/landing/features/features";
 import Cta from "@/components/landing/cta/cta";
 import Footer from "@/components/landing/footer/footer";
+import { motion } from "motion/react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen text-text-primary relative bg-gradient-to-b from-[#5865F2]/20 via-background to-[#1E293B]24">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.2 }}
+      className="min-h-screen text-text-primary relative bg-gradient-to-b from-[#5865F2]/20 via-background to-[#1E293B]24"
+    >
       <Hero />
       <Features />
       <Cta />
       <Footer />
-    </div>
+    </motion.div>
   );
 }
